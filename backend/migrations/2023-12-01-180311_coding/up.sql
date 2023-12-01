819c86b4225a1c68cd7ed4d6677e5f497ba827df
Adrian Filipek <adrqwer@outlook.com>
2023-12-01 20:05:02 +0100

    added users table

---
+++ backend/migrations/2023-12-01-180311_coding/up.sql
@@ -0,0 +1,12 @@
+-- Your SQL goes here
+CREATE TABLE todos (
+  id SERIAL PRIMARY KEY,
+  task VARCHAR NOT NULL,
+  done BOOLEAN NOT NULL DEFAULT 'f'
+);
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR NOT NULL UNIQUE,
+    password VARCHAR NOT NULL
+)
