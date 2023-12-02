9b29041fd06c84b29375986a936d226a0c2443fd
Bartek <barteksiedlarz12@gmail.com>
2023-12-02 03:42:30 +0100

    przod

---
+++ frontend/src/pages/components/PostsDisplay.tsx
@@ -0,0 +1,13 @@
+import Post from "./Post";
+
+const PostsDisplay = () => {
+    return(
+        <section className="bg-white dark:bg-gray-900">
+        <Post username="Cieciw"   title="przenocuje"
+    content="rudzy jest porabany dyuasgyufyusgfuisgyufausigfyuasguyfgasuifgyusgfuiysfuigsuifguaygfuiawguiawgf"
+    location="Warszawa"/>
+    </section>
+    )
+}
+
+export default PostsDisplay;
